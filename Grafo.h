55655941bd207e7d24b537e345ca84918ce7750c
Isaacgithub123 <isaacazofeifab@gmail.com>
2026-06-16 21:58:42 -0600

    Pair paera marcar indicar los pesos de las aristas

--- Grafo.h
+++ Grafo.h
@@ -1,5 +1,5 @@
 #pragma once
-#include <list>
+#include "LinkedList.h"
 
 using namespace std;
 
@@ -7,3 +7,15 @@ class Grafo {
 private:
     int cantidadNodos;
     list<int>* adyacencias;
+public:
+    Grafo(int n) {
+        cantidadNodos = n;
+        adyacencias = new list<int>[n];
+    }
+
+    ~Grafo() {
+        delete[] adyacencias;
+    }
+
+    void agregarArista(int origen, int destino) {
+     
